8a536e81f70c0b2eaaad23fd397424fe5787bbc4
unknown <swapnil227>
2021-02-26 15:42:38 +0530

    my first commit

---
+++ config.py
@@ -0,0 +1,13 @@
+#customhost = "employee.coghw13fheqo.us-east-2.rds.amazonaws.com"
+#customuser = "intellipaat"
+#custompass = "intellipaat123"
+#customdb = "employee"
+#custombucket = "addemployee"
+#customregion = "us-east-2"
+
+customhost = "employee.cuvxoblh4luj.us-east-2.rds.amazonaws.com"
+customuser = "swapnil"
+custompass = "swapnil22"
+customdb = "employee"
+custombucket = "employeess"
+customregion = "us-east-2"
